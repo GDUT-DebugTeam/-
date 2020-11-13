48eb33954cf162affbc94dfb5f88e52528e9dbfe
flamegun <70954932+flamegun@users.noreply.github.com>
2020-11-14 00:07:01 +0800

    Create ayu.java

---
+++ Mall-master/order_service_provider/ayu.java
@@ -0,0 +1,40 @@
+
+public static String getStringByUrl(String getUrl) {
+		// String getUrl =
+		StringBuffer sb = new StringBuffer();
+		InputStreamReader isr = null;
+		BufferedReader br = null;
+		try {
+			URL url = new URL(getUrl);
+			URLConnection urlConnection = url.openConnection();
+			urlConnection.setReadTimeout(60000);
+			urlConnection.setAllowUserInteraction(false);
+			/*isr = new InputStreamReader(url.openStream());*/
+			isr = new InputStreamReader(url.openStream(),Charset.forName("UTF-8"));
+			br = new BufferedReader(isr);
+			String line;
+			while ((line = br.readLine()) != null) {
+				sb.append(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (isr != null) {
+				try {
+					isr.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		return sb.toString();
+	}
